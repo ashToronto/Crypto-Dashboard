380215f6741dc22c3cd88681f6c9493951aaad7b
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-10-28 17:19:18 +0000

    created list of most popular with historical data rendering, fine tuned front end and css

--- src/components/Cryptocurrency.js
+++ src/components/Cryptocurrency.js
@@ -1,36 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {MdInsertChart, MdExposureNeg1, MdArrowDownward, MdArrowUpward} from 'react-icons/md';
 
 class Cryptocurrency extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state = {currencies: []}
+    this.state = {
+      currencies: [],
+      coin: "",
+    }
   }
 
-    // Call to cryptocompare API for top 100 coins listed
-    componentDidMount() {
-      fetch(`https://api.coinmarketcap.com/v2/ticker/?convert=USD&limit=100&sort=rank&structure=array`)
-      .then(res => res.json())
-      .then(result => {
-        this.setState({currencies: result.data})
-      });
-    }
+  // Call to cryptocompare API for top 100 coins listed
+  componentDidMount() {
+    fetch(`https://api.coinmarketcap.com/v2/ticker/?convert=USD&limit=100&sort=rank&structure=array`).then(res => res.json()).then(result => {
+      console.log("iii: ", result)
+      this.setState({currencies: result.data})
+    });
+  }
 
-    render(){
-      return (
-        <div>
-          Hello World
-          <table>
-            {this.state.currencies.map((currency, i) => {
-              return (
-                <tr>
-                  <p>{currency.name}</p>
-                  <p>{currency.quotes.USD.price}</p>
-                </tr>
-                );
-            })};
-            </table>
-        </div>
-      );
-    }
+  historical_data = (e) => {
+    e.preventDefault();
+    const coin = e.target.elements.current_coin.value
+    console.log("coin", coin)
+    fetch(`https://min-api.cryptocompare.com/data/histoday?fsym=${coin}&tsym=USD`).then(res => res.json()).then(result => {
+      console.log("historical data: ", result)
+    });
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+  render() {
+    return (<div className='currency_list'>
+      <h1>CryptoTrack</h1>
+      <p>Most Popular Cryptocurrencies</p>
+      <div className='wrapper'>
+        {
+          this.state.currencies.map((currency, i) => {
+            return (<div className='box' key={i}>
+              <img src={'https://s2.coinmarketcap.com/static/img/coins/16x16/' + currency.id + '.png'}/>
+              <p>{currency.name}</p>
+              <p>{currency.symbol}</p>
+              <p>$ {Math.round(currency.quotes.USD.price * 100) / 100}</p>
+              <span classname="last24h_value">{
+                  (currency.quotes.USD.percent_change_24h > 0)
+                    ? <font color="#04d67b">
+                        {currency.quotes.USD.percent_change_24h + '%'}
+                        <MdArrowUpward/></font>
+                    : <font color="#e50404">
+                        {currency.quotes.USD.percent_change_24h + '%'}
+                        <MdArrowDownward/></font>
+                }
+              </span>
+              <form className="icon_position" onSubmit={this.historical_data}>
+                <input type="hidden" name='current_coin' value={currency.symbol}/>
+                <button className="chart_bttn"><MdInsertChart/></button>
+              </form>
+            </div>);
+          })
+        };
+      </div>
+    </div>);
+  }
 }
 export default Cryptocurrency;
